4077ad02013573c26bbe20ac6c0be57f949fdb1f
M-Cooke <micahcooke75@gmail.com>
2016-11-28 10:45:42 -0600

    Update imageEdit.py

--- Assignments/program_3/imageEdit.py
+++ Assignments/program_3/imageEdit.py
@@ -1,1 +1,24 @@
+from PIL import Image
 
+
+class ImageEd(object):
+  def __init__(self):
+    pass
+  
+  def glass_effect(self):
+    pass
+  
+  def flip(self):
+    pass
+  
+  def blur(self):
+    pass
+  
+  def posterize(self):
+    pass
+  
+  def solarize(self):
+    pass
+  
+  def warhol(self):
+    pass
