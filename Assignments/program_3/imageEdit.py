21ec2cb38d703887a490155a8724c9b0e3f733a6
M-Cooke <micahcooke75@gmail.com>
2016-11-28 11:04:55 -0600

    Update imageEdit.py

--- Assignments/program_3/imageEdit.py
+++ Assignments/program_3/imageEdit.py
@@ -11,13 +11,31 @@ class ImageEd(object):
   def flip(self):
     pass
   
-  def blur(self):
-    pass
+  def blur(self, img, blur_power = 5):
+    width = img.size[0]
+    height = img.size[1]
+    
+    r = 0
+    g = 0
+    b = 0 
+    d = 2*blur_power * 2*blur_power
+    for x in range(blur_power, width-blur_power):
+      for y in range(blur_power, height-blur_power):
+        for i in range(-blur_power, blur_power):
+          for j in range(-blur_power, blur_power):
+            pix = img.getpixel((x+i, y+j))
+            r += pix[0]
+            g += pix[1]
+            b += pix[2]
+        img.putpixel((x,y),(int(r/d), int(g/d), int(b/d)))
+        r = 0
+        g = 0
+        b = 0
   
   def posterize(self):
     pass
   
-  def solarize(self):
+  def solarize(self, thresh = 3):
     pass
   
   def warhol(self):
